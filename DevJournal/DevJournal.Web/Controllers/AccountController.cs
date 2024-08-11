2dad6bb31f58594138ce91937fc903416ccc2479
JemReal <jemreal25@gmail.com>
2024-08-11 12:19:37 +0800

    Added user login and logout functionality

--- DevJournal/DevJournal.Web/Controllers/AccountController.cs
+++ DevJournal/DevJournal.Web/Controllers/AccountController.cs
@@ -7,10 +7,13 @@ namespace DevJournal.Web.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
 
-        public AccountController(UserManager<IdentityUser> userManager)
+        public AccountController(UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager)
         {
             this.userManager = userManager;
+            this.signInManager = signInManager;
         }
 
         [HttpGet]
@@ -44,6 +47,34 @@ namespace DevJournal.Web.Controllers
 
             // Show error notification
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            var signInResult = await signInManager.PasswordSignInAsync(loginViewModel.Username,
+                loginViewModel.Password, false, false);
+
+            if (signInResult != null && signInResult.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Show errors
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ DevJournal/DevJournal.Web/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DevJournal.Web.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
